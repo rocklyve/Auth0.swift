13ce31376ec766726e1d8cc74174c1272b52a1f0
David Laubenstein <david.laubenstein@posteo.de>
2019-04-10 10:15:48 +0200

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,16 @@
-// swift-tools-version:5.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "Auth0.swift",
-    products: [],
-    dependencies: [
-        .package(url: "https://github.com/rocklyve/SimpleKeychain.git", .branch("accioSupport")),
-        
+    name: "Auth0",
+    // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    products: [
+        .library(name: "Auth0", targets: ["Auth0"])
     ],
     targets: [
         .target(
             name: "Auth0",
-            dependencies: [
-                "SimpleKeychain",
-            ],
-            path: "App"
-        ),
+            path: "Auth0"
+        )
     ]
 )
