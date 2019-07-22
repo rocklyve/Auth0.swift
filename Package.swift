d2a32ef06586e3ca23b11afdf14dc9b6f6bb2ea9
David Laubenstein <david.laubenstein@posteo.de>
2019-07-22 16:35:37 +0200

    update version to 11

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Auth0",
-    // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
+    // platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
     products: [
         .library(name: "Auth0", targets: ["Auth0"])
     ],
